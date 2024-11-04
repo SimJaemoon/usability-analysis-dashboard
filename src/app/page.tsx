7ab5aa1eb76cd8813799f4e9f4ef471dcb910fce
SimJaemoon <limun1324@gmail.com>
2024-11-04 15:34:09 +0900

    UI: Add Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,58 @@ import styles from './page.module.scss';
 export default function Home() {
   return (
     <>
-      <header className={styles.header}>
-        <span>violet</span>
+      <header className={styles['header-content']}>
+        <span>Usability</span>
+        <br />
+        <span>Analysis</span>
+        <br />
+        <span>Dashboard</span>
       </header>
-      <main></main>
+      <main className={styles['main-content']}>
+        <div className={styles.description}>
+          데이터를 복사해 붙여넣어 주세요
+        </div>
+        <form className={styles['form-content']}>
+          <div className={styles.content}>
+            <div className={styles.spreadsheet} />
+            <div className={styles['divider-spreadsheet']} />
+            <div
+              className={`${styles.variable} ${styles['variable-comparison']}`}
+            >
+              <div>비교 대상</div>
+              <ul>
+                {Array.from({ length: 5 }, (_, i) => (
+                  <li key={i}>
+                    <button>{'item ' + i}</button>
+                  </li>
+                ))}
+              </ul>
+              <button className={styles['variable-more']}>▼</button>
+            </div>
+            <div className={styles['divider-variable']} />
+            <div
+              className={`${styles.variable} ${styles['variable-measurement']}`}
+            >
+              <div>측정 지표</div>
+              <ul>
+                {Array.from({ length: 5 }, (_, i) => (
+                  <li key={i}>
+                    <button>{'item ' + i}</button>
+                  </li>
+                ))}
+              </ul>
+              <button className={styles['variable-more']}>▼</button>
+            </div>
+          </div>
+          <div className={styles['sticky-button-placeholder']}></div>
+          <div className={styles['sticky-button-container']}>
+            <button className={styles['sticky-button-more']}>▼</button>
+            <button className={styles['sticky-button-analysis']}>
+              분석하기
+            </button>
+          </div>
+        </form>
+      </main>
       <footer></footer>
     </>
   );
