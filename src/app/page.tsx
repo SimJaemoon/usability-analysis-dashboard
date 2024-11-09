8d5fac26b965a396228447e3b5cf2feb89767798
SimJaemoon <limun1324@gmail.com>
2024-11-09 16:43:46 +0900

    feat: Add useEffect to use querySelector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import VariableList from '@/components/VariableList';
 
 import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Landing() {
   const searchParams = useSearchParams();
@@ -13,17 +14,66 @@ export default function Landing() {
 
   const router = useRouter();
 
-  const $html = document.querySelector('html');
-  if ($html !== null) {
+  useEffect(() => {
+    const $html = document.querySelector('html');
     if (isAnalysed) {
-      $html.classList.add('html-background-blackboard');
+      $html?.classList.add('html-background-blackboard');
     } else {
-      $html.classList.remove('html-background-blackboard');
+      $html?.classList.remove('html-background-blackboard');
     }
-  }
+  }, [isAnalysed]);
 
   return (
     <>
+      {isAnalysed && (
+        <>
+          <header>
+            <div>비교 대상</div>
+            <div>측정 지표</div>
+            <div>back BTN</div>
+          </header>
+          <main>
+            <article>
+              <h3>
+                <div>1. 개선 상태</div>
+                <button>▲</button>
+              </h3>
+              {/* 기술 통계 */}
+              <div>
+                <div>
+                  <button>기술 통계 ▲</button>-<button>대상 수준</button>-
+                  <button>측정 지표</button>
+                </div>
+                <div>
+                  <div>
+                    <div>Before / 과업 완료 시간</div>
+                    <div>
+                      <div>
+                        <div>표본 수</div>
+                        <div>107 (결측치 : 7)</div>
+                      </div>
+                      <div>
+                        <div>평균</div>
+                        <div>11.2</div>
+                      </div>
+                      <div>
+                        <div>표준 편차</div>
+                        <div>3.1</div>
+                      </div>
+                    </div>
+                    <figure>
+                      <figcaption>추후 미표시</figcaption>
+                      <div>Graph</div>
+                    </figure>
+                  </div>
+                </div>
+              </div>
+              {/* 평균 비교 */}
+              <div>평균 비교</div>
+            </article>
+          </main>
+        </>
+      )}
       {!isAnalysed && (
         <>
           <header className={styles['header-content']}>
